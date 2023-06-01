ee7c7b629270cbe5a77d7bffc956c741954e7c0e
melxin <melxin@tutamail.com>
2023-06-01 22:11:38 +0200

    project: Bump versions

--- buildSrc/src/main/kotlin/Dependencies.kt
+++ buildSrc/src/main/kotlin/Dependencies.kt
@@ -25,11 +25,11 @@
 
 object ProjectVersions {
     const val launcherVersion = "1.0.0"
-    const val rlVersion = "1.10.1"
+    const val rlVersion = "1.10.3"
 
     const val openosrsVersion = "4.31.2"
 
-    const val rsversion = 213
+    const val rsversion = 214.3
     const val cacheversion = 165
 
     const val lombokVersion = "1.18.22"
