8c6b5f0fb8ce68a66db64afa23d78f4a897954e1
melxin <melxin@tutamail.com>
2025-04-11 09:39:48 +0200

    rl-client: disable compiler control
    7a1b86d3e5d784f34946fde4ecd4cb985b0f1803 d32d1c0bd73a3a69e08bb768b3e0a697deb0f32c

--- runelite-client/src/main/java/net/runelite/client/RuneLite.java
+++ runelite-client/src/main/java/net/runelite/client/RuneLite.java
@@ -472,7 +472,7 @@ public class RuneLite
 		injector.injectMembers(client);
 
 		setupSystemProps();
-		setupCompilerControl();
+		//setupCompilerControl();
 
 		// Start the applet
 		copyJagexCache();
