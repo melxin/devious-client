arbahb/ulu-p

timers buffs: add purging staff effect to mark of darkness

runelite-client/src/main/java/net/runelite/client/plugins/timersandbuffs/TimersAndBuffsPlugin.java
@@ -979,7 +979,7 @@ public class TimersAndBuffsPlugin extends Plugin
 			}
 			else if (message.endsWith(MARK_OF_DARKNESS_MESSAGE))
 			{
-				createGameTimer(MARK_OF_DARKNESS, Duration.of(magicLevel, RSTimeUnit.GAME_TICKS));
+				createGameTimer(MARK_OF_DARKNESS, Duration.of(getMagicLevelMoD(magicLevel), RSTimeUnit.GAME_TICKS));
 			}
 			else if (message.contains(RESURRECT_THRALL_MESSAGE_START) && message.endsWith(RESURRECT_THRALL_MESSAGE_END))
 			{
@@ -998,13 +998,10 @@ public class TimersAndBuffsPlugin extends Plugin
 			}
 		}
 
-		if (config.showArceuusCooldown())
+		if (config.showArceuusCooldown() && message.endsWith(MARK_OF_DARKNESS_MESSAGE))
 		{
-			final int magicLevel = client.getRealSkillLevel(Skill.MAGIC);
-			if (message.endsWith(MARK_OF_DARKNESS_MESSAGE))
-			{
-				createGameTimer(MARK_OF_DARKNESS_COOLDOWN, Duration.of(magicLevel - 10, RSTimeUnit.GAME_TICKS));
-			}
+			final int magicLevelMoD = getMagicLevelMoD(client.getRealSkillLevel(Skill.MAGIC));
+			createGameTimer(MARK_OF_DARKNESS_COOLDOWN, Duration.of(magicLevelMoD - 10, RSTimeUnit.GAME_TICKS));
 		}
 
 		if (TZHAAR_PAUSED_MESSAGE.matcher(message).find())
@@ -1074,6 +1071,20 @@ public class TimersAndBuffsPlugin extends Plugin
 		{
 			createGameTimer(LIQUID_ADRENALINE);
 		}
+	}
+
+	private int getMagicLevelMoD(int magicLevel)
+	{
+		final ItemContainer container = client.getItemContainer(InventoryID.EQUIPMENT);
+		if (container != null)
+		{
+			final Item weapon = container.getItem(EquipmentInventorySlot.WEAPON.getSlotIdx());
+			if (weapon != null && weapon.getId() == ItemID.PURGING_STAFF)
+			{
+				return magicLevel * 5;
+			}
+		}
+		return magicLevel;
 	}
 
 	private boolean isInFightCaves()

melxin/devious-client

(cherry picked from commit 94ad678eac3146cbcad8bccbc0f184f46e6978e1)

runelite-client/src/main/java/net/runelite/client/plugins/timersandbuffs/TimersAndBuffsPlugin.java
@@ -996,7 +996,7 @@ public class TimersAndBuffsPlugin extends Plugin
 			}
 			else if (message.endsWith(MARK_OF_DARKNESS_MESSAGE))
 			{
-				createGameTimer(MARK_OF_DARKNESS, Duration.of(magicLevel, RSTimeUnit.GAME_TICKS));
+				createGameTimer(MARK_OF_DARKNESS, Duration.of(getMagicLevelMoD(magicLevel), RSTimeUnit.GAME_TICKS));
 			}
 			else if (message.contains(RESURRECT_THRALL_MESSAGE_START) && message.endsWith(RESURRECT_THRALL_MESSAGE_END))
 			{
@@ -1017,8 +1017,8 @@ public class TimersAndBuffsPlugin extends Plugin
 
 		if (message.endsWith(MARK_OF_DARKNESS_MESSAGE) && config.showArceuusCooldown())
 		{
-			final int magicLevel = client.getRealSkillLevel(Skill.MAGIC);
-			createGameTimer(MARK_OF_DARKNESS_COOLDOWN, Duration.of(magicLevel - 10, RSTimeUnit.GAME_TICKS));
+			final int magicLevelMoD = getMagicLevelMoD(client.getRealSkillLevel(Skill.MAGIC));
+			createGameTimer(MARK_OF_DARKNESS_COOLDOWN, Duration.of(magicLevelMoD - 10, RSTimeUnit.GAME_TICKS));
 		}
 
 		if (TZHAAR_PAUSED_MESSAGE.matcher(message).find())
@@ -1088,6 +1088,20 @@ public class TimersAndBuffsPlugin extends Plugin
 		{
 			createGameTimer(LIQUID_ADRENALINE);
 		}
+	}
+
+	private int getMagicLevelMoD(int magicLevel)
+	{
+		final ItemContainer container = client.getItemContainer(InventoryID.WORN);
+		if (container != null)
+		{
+			final Item weapon = container.getItem(EquipmentInventorySlot.WEAPON.getSlotIdx());
+			if (weapon != null && weapon.getId() == ItemID.PURGING_STAFF)
+			{
+				return magicLevel * 5;
+			}
+		}
+		return magicLevel;
 	}
 
 	private boolean isInFightCaves()