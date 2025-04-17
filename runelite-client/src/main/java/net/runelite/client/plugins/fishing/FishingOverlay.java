runelite/runelite

fishing: add trailblazer and trailblazer reloaded harpoons
Co-authored-by: Macweese <50101641+Macweese@users.noreply.github.com>

runelite-client/src/main/java/net/runelite/client/plugins/fishing/FishingOverlay.java
@@ -66,7 +66,10 @@ class FishingOverlay extends OverlayPanel
 		AnimationID.FISHING_DRAGON_HARPOON_OR,
 		AnimationID.FISHING_HARPOON,
 		AnimationID.FISHING_INFERNAL_HARPOON,
+		AnimationID.FISHING_INFERNAL_TRAILBLAZER_HARPOON,
+		AnimationID.FISHING_INFERNAL_TRAILBLAZER_RELOADED_HARPOON,
 		AnimationID.FISHING_TRAILBLAZER_HARPOON,
+		AnimationID.FISHING_TRAILBLAZER_RELOADED_HARPOON,
 		AnimationID.FISHING_KARAMBWAN,
 		AnimationID.FISHING_NET,
 		AnimationID.FISHING_OILY_ROD,

runelite-client/src/main/java/net/runelite/client/plugins/fishing/FishingPlugin.java
@@ -251,8 +251,11 @@ public class FishingPlugin extends Plugin
 				case ItemID.DRAGON_HARPOON:
 				case ItemID.DRAGON_HARPOON_OR:
 				case ItemID.INFERNAL_HARPOON:
+				case ItemID.INFERNAL_HARPOON_OR:
+				case ItemID.INFERNAL_HARPOON_OR_30342:
 				case ItemID.INFERNAL_HARPOON_UNCHARGED:
 				case ItemID.INFERNAL_HARPOON_UNCHARGED_25367:
+				case ItemID.INFERNAL_HARPOON_UNCHARGED_30343:
 				case ItemID.HARPOON:
 				case ItemID.BARBTAIL_HARPOON:
 				case ItemID.BIG_FISHING_NET:
@@ -270,7 +273,6 @@ public class FishingPlugin extends Plugin
 				case ItemID.KARAMBWAN_VESSEL_3159:
 				case ItemID.CORMORANTS_GLOVE:
 				case ItemID.CORMORANTS_GLOVE_22817:
-				case ItemID.INFERNAL_HARPOON_OR:
 				case ItemID.ECHO_HARPOON:
 				case ItemID.CRYSTAL_HARPOON:
 				case ItemID.CRYSTAL_HARPOON_23864:

melxin/devious-client

(cherry picked from commit ff3741d6e9bd181c25e08013f2a6e5d188fe0df4)

runelite-client/src/main/java/net/runelite/client/plugins/fishing/FishingOverlay.java
@@ -66,7 +66,10 @@ class FishingOverlay extends OverlayPanel
 		AnimationID.HUMAN_HARPOON_TRAILBLAZER_NO_INFERNAL,
 		AnimationID.HUMAN_HARPOON,
 		AnimationID.HUMAN_HARPOON_INFERNAL,
+		AnimationID.HUMAN_HARPOON_TRAILBLAZER,
+		AnimationID.HUMAN_HARPOON_TRAILBLAZER_RELOADED,
 		AnimationID.HUMAN_HARPOON_LEAGUE_TRAILBLAZER,
+		AnimationID.HUMAN_HARPOON_TRAILBLAZER_RELOADED_NO_INFERNAL,
 		AnimationID.HUMAN_OCTOPUS_POT,
 		AnimationID.HUMAN_SMALLNET,
 		AnimationID.HUMAN_FISHING_CASTING,

runelite-client/src/main/java/net/runelite/client/plugins/fishing/FishingPlugin.java
@@ -250,8 +250,11 @@ public class FishingPlugin extends Plugin
 				case ItemID.DRAGON_HARPOON:
 				case ItemID.TRAILBLAZER_HARPOON_NO_INFERNAL:
 				case ItemID.INFERNAL_HARPOON:
+				case ItemID.TRAILBLAZER_HARPOON:
+				case ItemID.TRAILBLAZER_RELOADED_HARPOON:
 				case ItemID.INFERNAL_HARPOON_EMPTY:
 				case ItemID.TRAILBLAZER_HARPOON_EMPTY:
+				case ItemID.TRAILBLAZER_RELOADED_HARPOON_EMPTY:
 				case ItemID.HARPOON:
 				case ItemID.HUNTING_BARBED_HARPOON:
 				case ItemID.BIG_NET:
@@ -269,7 +272,6 @@ public class FishingPlugin extends Plugin
 				case ItemID.TBWT_KARAMBWAN_VESSEL_LOADED_WITH_KARAMBWANJI:
 				case ItemID.AERIAL_FISHING_GLOVES_NO_BIRD:
 				case ItemID.AERIAL_FISHING_GLOVES_BIRD:
-				case ItemID.TRAILBLAZER_HARPOON:
 				case ItemID.LEAGUE_TRAILBLAZER_HARPOON:
 				case ItemID.CRYSTAL_HARPOON:
 				case ItemID.GAUNTLET_HARPOON: