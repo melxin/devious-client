runelite/runelite

mining: add trailblazer reloaded pickaxes
Co-authored-by: Macweese <50101641+Macweese@users.noreply.github.com>

runelite-client/src/main/java/net/runelite/client/plugins/mining/MiningOverlay.java
@@ -57,11 +57,14 @@ class MiningOverlay extends OverlayPanel
 		AnimationID.MINING_MOTHERLODE_DRAGON_UPGRADED,
 		AnimationID.MINING_MOTHERLODE_GILDED,
 		AnimationID.MINING_MOTHERLODE_INFERNAL,
+		AnimationID.MINING_MOTHERLODE_INFERNAL_TRAILBLAZER,
+		AnimationID.MINING_MOTHERLODE_INFERNAL_TRAILBLAZER_RELOADED,
 		AnimationID.MINING_MOTHERLODE_IRON,
 		AnimationID.MINING_MOTHERLODE_MITHRIL,
 		AnimationID.MINING_MOTHERLODE_RUNE,
 		AnimationID.MINING_MOTHERLODE_STEEL,
-		AnimationID.MINING_MOTHERLODE_TRAILBLAZER);
+		AnimationID.MINING_MOTHERLODE_TRAILBLAZER,
+		AnimationID.MINING_MOTHERLODE_TRAILBLAZER_RELOADED);
 
 	private final Client client;
 	private final MiningPlugin plugin;

melxin/devious-client

(cherry picked from commit cd3859b870d1a85ef1d02adee54436b7a0f97a09)

runelite-client/src/main/java/net/runelite/client/plugins/mining/MiningOverlay.java
@@ -57,11 +57,14 @@ class MiningOverlay extends OverlayPanel
 		AnimationID.HUMAN_MINING_DRAGON_PICKAXE_PRETTY_WALL,
 		AnimationID.HUMAN_MINING_GILDED_PICKAXE_WALL,
 		AnimationID.HUMAN_MINING_INFERNAL_PICKAXE_WALL,
+		AnimationID.HUMAN_MINING_TRAILBLAZER_PICKAXE_WALL,
+		AnimationID.HUMAN_MINING_TRAILBLAZER_RELOADED_PICKAXE_WALL,
 		AnimationID.HUMAN_MINING_IRON_PICKAXE_WALL,
 		AnimationID.HUMAN_MINING_MITHRIL_PICKAXE_WALL,
 		AnimationID.HUMAN_MINING_RUNE_PICKAXE_WALL,
 		AnimationID.HUMAN_MINING_STEEL_PICKAXE_WALL,
-		AnimationID.HUMAN_MINING_LEAGUE_TRAILBLAZER_PICKAXE_WALL);
+		AnimationID.HUMAN_MINING_LEAGUE_TRAILBLAZER_PICKAXE_WALL,
+		AnimationID.HUMAN_MINING_TRAILBLAZER_RELOADED_PICKAXE_NO_INFERNAL_WALL);
 
 	private final Client client;
 	private final MiningPlugin plugin;