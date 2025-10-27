ef206d6e4e81c77bd8770028209fda4a9634888a
Blonks <40224407+Blonks-afk@users.noreply.github.com>
2025-10-26 23:32:41 -0400

    minimap: fix npe on startup (#19481)
    (cherry picked from commit 670c4a0aa012477e3a3c8488c5442ae6fe96dd77)

--- runelite-client/src/main/java/net/runelite/client/plugins/minimap/MinimapPlugin.java
+++ runelite-client/src/main/java/net/runelite/client/plugins/minimap/MinimapPlugin.java
@@ -135,6 +135,11 @@ public class MinimapPlugin extends Plugin
 
 	private void updateMinimapWidgetVisibility(boolean hide)
 	{
+		if (client.getGameState() != GameState.LOGGED_IN)
+		{
+			return;
+		}
+
 		boolean vanillaHideMinimap = client.getVarbitValue(VarbitID.MINIMAP_TOGGLE) == 1;
 
 		setHidden(InterfaceID.ToplevelOsrsStretch.MAP_MINIMAP, hide || vanillaHideMinimap);
